23581fc3782b0525731ebee43c3f52b8834b248b
Edmund Ham <hamsu2959@gmail.com>
2019-02-25 17:03:31 -0800

    added pdf generator code in app component

--- Spa/ClientApp/src/app/app.component.ts
+++ Spa/ClientApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  //pdf generator
+  download() {
+    var data = document.getElementById('ConvertibleContent');
+    html2canvas(data).then(canvas => {
+      const contentDataURL = canvas.toDataURL('image/png')
+      var imageWidth = 208;
+      var imageHeight = canvas.height * imageWidth / canvas.width;
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF   
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, imageWidth, imageHeight)
+      pdf.save('test.pdf'); // Generated PDF   
+    });
+  }
 }
