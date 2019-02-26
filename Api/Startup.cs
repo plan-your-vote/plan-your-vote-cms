b7a7f6e89fba5702ad7799a7296f58e4d0821b23
Noah Seltzer <noahseltzer@outlook.com>
2019-02-25 21:48:25 -0800

    swagger is imported

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace Api
 {
@@ -26,6 +28,9 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Info { Title = "VotingTool API", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +47,15 @@ namespace Api
             }
 
             app.UseHttpsRedirection();
+            
+            //Swagger boilerplate
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "VotingTool API V1");
+            });
+
+
             app.UseMvc();
         }
     }
