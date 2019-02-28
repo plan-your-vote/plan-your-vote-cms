b8d133378fba65699ae3ef54149c6abd738c9f0c
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-02-28 13:51:37 -0800

    Added components that were in previous build

--- Spa/ClientApp/src/app/app.module.ts
+++ Spa/ClientApp/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent
   ],
   imports: [
     BrowserModule,
