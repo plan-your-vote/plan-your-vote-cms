7db6f93f898ac6e927e0b83171d20f3271cff28c
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-03-04 20:44:05 -0800

    Move style switching elements to higher component

--- Spa/ClientApp/src/app/app.component.ts
+++ Spa/ClientApp/src/app/app.component.ts
@@ -1,20 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from './services/translate.service';
 import { PdfService } from './services/pdf.service';
+import { Election } from './models/election';
+import { ElectionService } from './services/election.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  data: Election[] = [];
+  currentElection: Election;
+  index: number;
 
   title = 'ClientApp';
 
-  constructor(private translate: TranslateService, private pdfService: PdfService) {
+  constructor(
+    private translate: TranslateService, 
+    private pdfService: PdfService,
+    private electionApi: ElectionService
+    ) {
     translate.use('en').then(() => {
       console.log(translate.data);
     });
+
+    this.index = 0;
+    this.electionApi.getElections()
+      .subscribe(res => {
+        this.data = res;
+      });
+  }
+
+  ngOnInit(): void {
+    this.nextElection();
+  }
+
+  public nextElection(): void {
+    this.currentElection = this.data[this.index];
+    if (this.index != this.data.length-1) {
+      this.index++;
+    } else {
+      this.index = 0;
+    }
   }
 
   setLang(lang: string) {
--- Spa/ClientApp/src/app/home/home.component.ts
+++ Spa/ClientApp/src/app/home/home.component.ts
@@ -1,37 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ElectionService } from '../services/election.service';
-import { Election } from '../models/election';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  data: Election[] = [];
-  currentElection: Election;
-  index: number;
   
-  constructor(
-    private electionApi: ElectionService
-  ) {
-    this.index = 0;
-    this.electionApi.getElections()
-      .subscribe(res => {
-        this.data = res;
-      });
-   }
+  constructor( ) { }
 
-  ngOnInit(): void {
-      this.nextElection();
-  }
-
-  public nextElection(): void {
-    this.currentElection = this.data[this.index];
-    if (this.index != this.data.length-1) {
-      this.index++;
-    } else {
-      this.index = 0;
-    }
-  }
+  ngOnInit(): void { }
 }
