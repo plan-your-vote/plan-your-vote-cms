f31fe3183c9fff653992494ee69c924741270e07
cameronlay <cameron.lay1@gmail.com>
2019-03-07 19:08:47 -0800

    convert icons to material design

--- Spa/ClientApp/src/app/footer-menu/footer-menu.component.ts
+++ Spa/ClientApp/src/app/footer-menu/footer-menu.component.ts
@@ -13,15 +13,24 @@ export class FooterMenuComponent implements OnInit {
 
   languages: string[] = [];
   defaultLanguage: string = window.navigator.language.includes("en-") ? "en" : window.navigator.language;
+  candidates: Candidate[] = [];
+  description: string;
 
   constructor(public share: ShareService,
-    private translateService: TranslateService,
-    @Inject(LOCALE_ID) public locale: string) {
+              private translateService: TranslateService,
+              private candidateService: CandidateService,
+              @Inject(LOCALE_ID) public locale: string) {
     this.languages = translateService.languages;
     // translateService.use(this.defaultLanguage);
   }
 
+  getCandidates(): void {
+    this.candidateService.getCandidates().subscribe(data => (this.candidates = data));
+  }
+
   ngOnInit() {
+    this.getCandidates();
+    this.description = this.candidates[0].firstName;
   }
 
   onLangChange(event) {
