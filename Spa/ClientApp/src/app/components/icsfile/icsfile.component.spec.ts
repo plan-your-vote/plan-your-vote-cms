8b354f709287e0dd6dd9eb3996b2bf31fd112dfe
Hank <cchang189@my.bcit.ca>
2019-03-08 05:16:36 -0800

    icsfile
    A button to add ics file

---
+++ Spa/ClientApp/src/app/components/icsfile/icsfile.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IcsfileComponent } from './icsfile.component';
+
+describe('IcsfileComponent', () => {
+  let component: IcsfileComponent;
+  let fixture: ComponentFixture<IcsfileComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ IcsfileComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IcsfileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
