136af4e4fb7fdc28226897ec95984169f3a0ae9b
Cameron Lay <cameron.lay1@gmail.com>
2019-03-08 15:38:36 -0800

    update url string for api client

--- Spa/ClientApp/src/app/services/candidate.service.ts
+++ Spa/ClientApp/src/app/services/candidate.service.ts
@@ -3,8 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Candidate } from 'src/app/models/candidate';
 
-// const clientsUrl = "https://localhost:5001/api/candidates";              //DEV AND TESTING
-const clientsUrl = "http://vote-web.azurewebsites.net/api/candidates"     //PRODUCTION
+const clientsUrl = "https://localhost:5001/api/candidates";              //DEV AND TESTING
+// const clientsUrl = "http://vote-web.azurewebsites.net/api/candidates"     //PRODUCTION
 
 @Injectable({
   providedIn: 'root'
