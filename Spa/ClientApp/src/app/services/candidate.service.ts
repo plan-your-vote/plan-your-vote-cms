a63d9bc8b767595f2b13a87b176e6fe42e72d622
Cameron Lay <cameron.lay1@gmail.com>
2019-03-08 15:52:08 -0800

    remove unused variables within candidate service

--- Spa/ClientApp/src/app/services/candidate.service.ts
+++ Spa/ClientApp/src/app/services/candidate.service.ts
@@ -2,15 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Candidate } from 'src/app/models/candidate';
 
-const clientsUrl = "https://localhost:5001/api/candidates";              //DEV AND TESTING
-// const clientsUrl = "http://vote-web.azurewebsites.net/api/candidates"     //PRODUCTION
+// const clientsUrl = "https://localhost:5001/api/candidates";              //DEV AND TESTING
+const clientsUrl = "http://cityvote.azurewebsites.net/api/candidates"     //PRODUCTION
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidateService {
   candidates = [];
-  candidate;
 
   constructor(private http: HttpClient) {}
 
