7cbe7b7b7844e8802a2c19e1ac0416b66934b4a7
tommyyeh0505 <32403423+tommyyeh0505@users.noreply.github.com>
2019-03-25 05:22:12 -0700

    added email validation and error messages

--- Spa/ClientApp/src/app/components/email/email.component.ts
+++ Spa/ClientApp/src/app/components/email/email.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { EmailService } from "src/app/services/email.service";
+import { invalid } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: "app-email",
@@ -10,12 +11,32 @@ export class EmailComponent implements OnInit {
   emailaddress: string;
   subject: string;
   message: string;
+  invalidemail: boolean = false;
+  noemail: boolean = false;
 
   constructor(private emailService: EmailService) {}
 
   ngOnInit() {}
 
   sendReminderEmail() {
-    this.emailService.sendReminderEmail(this.emailaddress, this.subject, this.message);
+    //EmailRegex
+    var regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    this.invalidemail = false;
+    this.noemail = false;
+
+    if (this.emailaddress == null || this.emailaddress.length == 0) {
+      this.noemail = true;
+      console.log("NO EMAIL");
+
+    } else if (regexp.test(this.emailaddress)) {
+      this.emailService.sendReminderEmail(this.emailaddress, this.subject, this.message);
+      console.log("EMAIL SENT");
+    } else{
+      //handle error
+      //send warning
+      this.invalidemail = true;
+      console.log("INVALID EMAIL");
+    }
+
   }
 }
