64bb8e1ec154fd9c81b8e2684ba173a18baded94
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-03-29 11:16:52 -0700

    Add new model classes

---
+++ VotingModelLibrary/Models/Election.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VotingModelLibrary.Models
+{
+    class Election
+    {
+        [Key]
+        public int ElectionId { get; set; }
+
+        public string DateStart { get; set; }
+        public string DateEnd { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ VotingModelLibrary/Models/PollingStation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VotingModelLibrary.Models
+{
+    class PollingStation
+    {
+        [Key]
+        public int PollingStationId { get; set; }
+        public int ElectionId { get; set; }
+        public Election Election { get; set; }
+
+        public string Name { get; set; }
+        public string AdditionalInfo { get; set; }
+        public string Address { get; set; }
+        public string WheelchairInfo { get; set; }
+        public string ParkingInfo { get; set; }
+        public string WashroomInfo { get; set; }
+        public string GeneralAccessInfo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitute { get; set; }
+    }
+}
