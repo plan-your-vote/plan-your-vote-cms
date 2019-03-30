939f23875a09324d87053fd2a9f6d4c6bf0019c2
Amy Hong <amyhong0502@gmail.com>
2019-03-30 13:29:39 -0700

    organized whitespace

--- Spa/ClientApp/src/app/components/candidate-list/candidate-list.component.ts
+++ Spa/ClientApp/src/app/components/candidate-list/candidate-list.component.ts
@@ -3,9 +3,9 @@ import { CandidateService } from '../../services/candidate.service';
 import { Candidate } from 'src/app/models/candidate';
 
 @Component({
-  selector   : 'app-candidate-list',
+  selector: 'app-candidate-list',
   templateUrl: './candidate-list.component.html',
-  styleUrls  : ['./candidate-list.component.less']
+  styleUrls: ['./candidate-list.component.less']
 })
 export class CandidateListComponent implements OnInit {
   title = 'candidates';
@@ -18,26 +18,20 @@ export class CandidateListComponent implements OnInit {
   }
 
   ngOnInit() {
-    if(!localStorage.getItem('candidates')) {
+    if (!localStorage.getItem('candidates')) {
       this.getCandidates();
     } else {
       this.candidates = JSON.parse(localStorage.getItem('candidates'));
     }
-    alert(this.candidates[0])
   }
 
   onSelect(c: Candidate) {
-    if(!c.selected) {
+    if (!c.selected) {
       c.selected = true;
-    
       localStorage.setItem('candidates', JSON.stringify(this.candidates));
     } else {
       c.selected = false;
       localStorage.setItem('candidates', JSON.stringify(this.candidates));
     }
-    
   }
-
-  
-
 }
