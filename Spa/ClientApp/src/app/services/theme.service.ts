d78c051a380ea816b3c94e3f348d49eb6ba3414c
Amy Hong <amyhong0502@gmail.com>
2019-03-29 20:14:21 -0700

    implemented service to get the user's choice of theme from backend

--- Spa/ClientApp/src/app/services/theme.service.ts
+++ Spa/ClientApp/src/app/services/theme.service.ts
@@ -1,9 +1,32 @@
 import { Inject, Injectable } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+const API_URL = "https://localhost:44307/api/themes/";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
-  constructor(@Inject(DOCUMENT) public document: Document) {}
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    private http: HttpClient
+  ) {}
+
+  private themeName: string;
+
+  getUserSelection() {
+    return new Promise<string>((resolve, reject) => {
+      this.http.get<string>(API_URL).subscribe(
+        res => {
+          this.themeName = res;
+          resolve(this.themeName);
+        },
+        error => {
+          this.themeName = '';
+          resolve(this.themeName);
+        }
+      );
+    });
+  }
 }
