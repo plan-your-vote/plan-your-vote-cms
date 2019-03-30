c61013bb2a36d84f7fc4b8323bd8af0a918cde7b
Felix Lin <felixhwlin@gmail.com>
2019-03-29 17:15:51 -0700

    Front-end unit-test modification

--- Spa/ClientApp/src/app/app.component.spec.ts
+++ Spa/ClientApp/src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
   it(`should have as title 'ClientApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
