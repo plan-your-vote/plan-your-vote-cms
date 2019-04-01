390f7e9f258b031cb26172f46b6723dbfa472c5f
noah-seltzer <noahseltzer@outlook.com>
2019-04-01 15:12:49 -0700

    added model classes back in

---
+++ Web/Models/Candidate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace VotingModelLibrary.Models
+{
+    public class Candidate
+    {
+        [Key]
+        public int CandidateId { get; set; }
+        public int ElectionId { get; set; }
+        public Election Election { get; set; }
+
+        [Display (Name="FirstName")]
+        public string FirstName { get; set; }
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+        [Display(Name = "Picture")]
+        public string Picture { get; set; }
+        [Display(Name = "Biography")]
+        public string Biography { get; set; }
+        [Display(Name = "OrganizationId")]
+        public int OrganizationId { get; set; }
+        [Display(Name = "Organization")]
+        public Organization Organization { get; set; }
+
+        public List<CandidateRace> CandidateRaces { get; set; }
+        public List<Contact> Contacts { get; set; }
+    }
+}
---
+++ Web/Models/Organization.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace VotingModelLibrary.Models
+{
+    public class Organization
+    {
+        [Key]
+        public int OrganizationId { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Candidate> Candidates { get; set; }
+    }
+}
