142d66f98ee9ba7906496c21d0afc62d0a2ea1cf
Amy Hong <amyhong0502@gmail.com>
2019-04-01 18:50:13 -0700

    removed unused component

--- Spa/ClientApp/src/app/app.module.ts
+++ Spa/ClientApp/src/app/app.module.ts
@@ -34,7 +34,6 @@ import { SelectionComponent } from './selection/selection.component';
 import { FooterMenuComponent } from './footer-menu/footer-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 export function setupTranslateFactory(service: TranslateService): Function {
   return () => service.use('en');
@@ -46,7 +45,6 @@ export function setupTranslateFactory(service: TranslateService): Function {
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
     TranslatePipe,
     CandidateItemComponent,
     CandidateListComponent,
@@ -91,7 +89,6 @@ export function setupTranslateFactory(service: TranslateService): Function {
       multi: true
     },
     JSONParserService
-    
   ],
   bootstrap: [AppComponent]
 })
