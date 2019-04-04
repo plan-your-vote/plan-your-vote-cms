d46347712f7e1bfd6d5f0a544970bd68bff37941
ryalia44 <Ryalia888@gmail.com>
2019-04-03 19:10:34 -0700

    abstracted candidate races table

--- Spa/ClientApp/src/app/selection/candidate-table/candidate-table.component.ts
+++ Spa/ClientApp/src/app/selection/candidate-table/candidate-table.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-candidate-table',
   templateUrl: './candidate-table.component.html',
-  styleUrls: ['./candidate-table.component.less']
+  styleUrls: ['./candidate-table.component.less'],
+  inputs: ['race']
 })
 export class CandidateTableComponent implements OnInit {
+
+  @Input() race;
 
   constructor() { }
 
