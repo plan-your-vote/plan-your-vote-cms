f8a11cafdf4630f9c5f7188b539af0ea88d168de
Elizabeth Lee <quotable01@gmail.com>
2019-05-17 21:03:54 -0700

    #63 Added Organizations controller & views

---
+++ Web/CmsControllers/OrganizationsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Models;
+
+namespace Web.CmsControllers
+{
+    public class OrganizationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrganizationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Organizations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Organizations.ToListAsync());
+        }
+
+        // GET: Organizations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organization = await _context.Organizations
+                .FirstOrDefaultAsync(m => m.OrganizationId == id);
+            if (organization == null)
+            {
+                return NotFound();
+            }
+
+            return View(organization);
+        }
+
+        // GET: Organizations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Organizations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OrganizationId,Name,Description")] Organization organization)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(organization);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(organization);
+        }
+
+        // GET: Organizations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organization = await _context.Organizations.FindAsync(id);
+            if (organization == null)
+            {
+                return NotFound();
+            }
+            return View(organization);
+        }
+
+        // POST: Organizations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OrganizationId,Name,Description")] Organization organization)
+        {
+            if (id != organization.OrganizationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(organization);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrganizationExists(organization.OrganizationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(organization);
+        }
+
+        // GET: Organizations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organization = await _context.Organizations
+                .FirstOrDefaultAsync(m => m.OrganizationId == id);
+            if (organization == null)
+            {
+                return NotFound();
+            }
+
+            return View(organization);
+        }
+
+        // POST: Organizations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var organization = await _context.Organizations.FindAsync(id);
+            _context.Organizations.Remove(organization);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrganizationExists(int id)
+        {
+            return _context.Organizations.Any(e => e.OrganizationId == id);
+        }
+    }
+}
