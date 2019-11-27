e50c198bf28996ce9e6a7caee11c435570fdfbc6
Joey Tang <joeyt@sfu.ca>
2019-11-26 17:03:03 -0800

    Removed unused 'using' statements in homecontroller

--- Web/CmsControllers/HomeController.cs
+++ Web/CmsControllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
